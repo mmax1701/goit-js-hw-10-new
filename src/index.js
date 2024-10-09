47cc0b07d12fd86fbc64db955b5ad02eadeacb92
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-10-09 22:10:19 +0300

    js

--- src/index.js
+++ src/index.js
@@ -2,29 +2,47 @@
 
 
 const select = document.querySelector('.breed-select')
+const info = document.querySelector('.cat-info')
 
-const ENDPOINT = 'https://api.thecatapi.com/v1/breeds';
+const URL = 'https://api.thecatapi.com/v1';
 const KEY = 'live_EJrN7e2BWq8nuTG6cxjyRu8hHhViUZv3d3f01D7MOLowcl2vTNvfHudaMaMhvVR8';
 
 select.addEventListener('change', onSelect)
 
-function onSelect() {
-    fetchBreeds(arr)
-    select.value = arr.id
-    console.log(arr);
+function onSelect(evt) {
+    const catId = evt.target.value;
+    fetchCatByBreed(catId)
 }
 
 function fetchBreeds() {
-   return fetch(`${ENDPOINT}?x-api-key=${KEY}`)
-        .then(resp => {
+   return fetch(`${URL}/breeds?x-api-key=${KEY}`)
+       .then(resp => {
             if (!resp.ok) {
-            throw new Error(resp.status)
+            throw new Error(resp.statusText)
             }
             return resp.json()
         })
-        .then(data => data.map((id) => console.log(id)))
-        .catch()
-    
 }
 
+fetchBreeds().then(data => data.map(elem => select.insertAdjacentHTML('beforeend', `<option value="${elem.id}">${elem.name}</option>`))).catch(err => console.log(err))
 
+
+function fetchCatByBreed(breedId) {
+    console.log(breedId);
+    fetch(`${URL}/images/search?breed_ids=${breedId}`)
+        .then(resp => {
+            if (!resp.ok) {
+            throw new Error(resp.statusText)
+            }
+            return resp.json();
+        }
+    )
+    //     .then(data => data.map(elem => (info.innerHTML =`<img src="${elem.url}" alt="#" width="300px">
+    // <h1></h1>
+    // <p></p>
+    // <p>Temperament:</p>`)))
+    //     .catch(err => console.log(err))
+
+    .then(data => console.log(data))
+        .catch(err => console.log(err))
+}
